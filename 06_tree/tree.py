85526d8aa2f40af2894ff29761e1575f8c0a139e
chungyau97 <chungyau97@gmail.com>
2024-03-30 09:27:27 +0800

    complete tree class functions

--- 06_tree/tree.py
+++ 06_tree/tree.py
@@ -4,21 +4,36 @@ class TreeNode:
         self.parent = None
         self.children = []
 
-def add_child(parent: TreeNode, child: TreeNode):
-    if child in parent.children:
-        print("child already exist.")
-        return
-    
-    child.parent = parent
-    parent.children.append(child)
+    def add_child(self, child):
+        if child in self.children:
+            print("child already exist.")
+            return
+        
+        child.parent = self
+        self.children.append(child)
 
-def print_tree(tree_node: TreeNode):
-    print(tree_node.data)
+    def level(self):
+        level = 0
+
+        while self.parent:
+            level += 1
+            self = self.parent
+
+        return level
+
+def print_start_at(tree_node: TreeNode):
+    
+    level = tree_node.level()
+    indentations = ''
+    if level != 0:
+        indentations = tree_node.level() * '  ' + '|__'
+
+    print(indentations + tree_node.data)
 
     for child in tree_node.children:
-        print_tree(child)
+        print_start_at(child)
 
-def print_root_tree(tree_node: TreeNode):
+def print_from_root(tree_node: TreeNode):
 
     while tree_node.parent:
         tree_node = tree_node.parent
@@ -27,21 +42,21 @@ def print_root_tree(tree_node: TreeNode):
     print(root_node.data)
 
     for child in root_node.children:
-        print_tree(child)
+        print_start_at(child)
 
 
 root_level = TreeNode('Nilupul')
 
 first_level = TreeNode('Chinmay')
-add_child(root_level, first_level)
-add_child(root_level, first_level)
+root_level.add_child(first_level)
+root_level.add_child(first_level)
 
 second_level = TreeNode('Vishwa')
-add_child(first_level, second_level)
+first_level.add_child(second_level)
 
 third_level = TreeNode('Dhaval')
-add_child(second_level, third_level)
+second_level.add_child(third_level)
 
-print_root_tree(third_level)
+print_from_root(third_level)
 
-print_tree(root_level)
+print_start_at(root_level)
